f9d0ceb770cfeb764bd337e5983dd0d1d349308c
raja-ashok <rashok.svks@gmail.com>
2019-01-01 12:12:22 +0530

    Parse and get XML tag value

--- src/ifl_conf_parser.c
+++ src/ifl_conf_parser.c
@@ -1,3 +1,6 @@
+#include <string.h>
+#include <ctype.h>
+
 #include "ifl_types.h"
 #include "ifl_conf_parser.h"
 #include "ifl_log.h"
@@ -10,13 +13,15 @@ int g_depth = 0;
 void XMLCALL start(void *data, const XML_Char *el, const XML_Char **attr)
 {
     int i;
-    (void)data;
 
     for (i = 0; i < g_depth; i++) {
         printf("  ");
     }
 
     printf("%s", el);
+    if (data) {
+        printf("=%s", (char *)data);
+    }
     for (i = 0; attr[i]; i += 2) {
         printf(" %s=%s", attr[i], attr[i + 1]);
     }
@@ -29,6 +34,40 @@ void XMLCALL end(void *data, const XML_Char *el)
     (void)data;
     (void)el;
     g_depth--;
+}
+
+char *trim_space(char *in)
+{
+    char *out = NULL;
+    int len;
+    if (in) {
+        len = strlen(in);
+        while(len && isspace(in[len - 1])) --len;
+        while(len && *in && isspace(*in)) ++in, --len;
+        if (len) {
+            out = strndup(in, len);
+        }
+    }
+    return out;
+}
+
+void XMLCALL data_handler(void *data, const XML_Char *buf, int len)
+{
+    char *buf_new;
+    char *buf_new2;
+    if (buf && (len > 1)) {
+        buf_new = calloc(1, len+1);
+        if (buf_new) {
+            memcpy(buf_new, buf, len);
+            buf_new2 = trim_space(buf_new);
+            free(buf_new);
+            buf_new = NULL;
+            if (buf_new2) {
+                printf(" [%s]\n", buf_new2);
+                free(buf_new2);
+            }
+        }
+    }
 }
 
 void IFL_ParseConf(const char *xml_file_name, const char *xml_content)
@@ -56,6 +95,7 @@ void IFL_ParseConf(const char *xml_file_name, const char *xml_content)
     }
 
     XML_SetElementHandler(xparser, start, end);
+    XML_SetCharacterDataHandler(xparser, data_handler);
 
     do {
         len = (int)fread(buf, 1, sizeof(buf) - 1, xml_file);
