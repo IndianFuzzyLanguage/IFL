575dc2a2dce7774463acb6043d3f5ba8dd805fef
raja-ashok <rashok.svks@gmail.com>
2019-01-13 11:44:51 +0530

    Include function name in log

--- src/ifl_log.h
+++ src/ifl_log.h
@@ -11,11 +11,11 @@ extern char *g_log_level_str[IFL_LOG_LEVEL_MAX];
 
 void IFL_LogFunc(uint8_t log_level, const char *format, ...);
 
-#define LOG_FUNC(log_level, file, line_num, format, ...) \
-    IFL_LogFunc((uint8_t)log_level, "[%s:%d][%s] "format"\n", file, line_num, \
-        g_log_level_str[log_level], ##__VA_ARGS__)
+#define LOG_FUNC(log_level, file, line_num, func, format, ...) \
+    IFL_LogFunc((uint8_t)log_level, "[%s][%s:%d][%s] "format"\n", g_log_level_str[log_level], \
+        file, line_num, func, ##__VA_ARGS__)
 
-#define LOG(log_level, format, ...) LOG_FUNC(log_level, __FILE__, __LINE__, format, ##__VA_ARGS__)
+#define LOG(level, fmt, ...) LOG_FUNC(level, __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
 
 #define ERR(format, ...) LOG(IFL_LOG_ERR, format, ##__VA_ARGS__)
 #define INFO(format, ...) LOG(IFL_LOG_INFO, format, ##__VA_ARGS__)
