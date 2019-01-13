378e549ebefee13e1da13539e3c691ed7e924bf8
raja-ashok <rashok.svks@gmail.com>
2019-01-13 23:58:47 +0530

    Add validation in host 2 network function

--- src/ifl_util.c
+++ src/ifl_util.c
@@ -9,6 +9,10 @@ void IFL_Host2Network(uint8_t *buf, uint32_t buf_size, uint32_t value)
 {
     int i;
     TRACE("Updating value=%u on %p", value, buf);
+    if (buf_size > sizeof(value)) {
+        ERR("Buf size =%u greater than max=%zu", buf_size, sizeof(value));
+        return;
+    }
     for (i = 0; i < buf_size; i++) {
         IFL_NthH2N(buf, buf_size, value, i);
         TRACE("At idx=%d %u", i, buf[i]);
